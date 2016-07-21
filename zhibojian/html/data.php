195716e4baa4573928e2525b6723e805243eb018
Salde <sujinw@qq.com>
2016-07-21 23:16:30 +0800

    font-icon

---
+++ zhibojian/html/data.php
@@ -0,0 +1,98 @@
+<?php
+
+$data = array(
+	"lists"=>array(
+		array(
+			"title"=>"用一年的时间，换一辈子精彩 拷贝111",
+			"imgSrc"=>"images/list.png",
+			"content"=>"11年传统旅游从业者为什么全力以赴...",
+			"teacher"=>"主讲：王凯丽"
+		),
+		array(
+			"title"=>"用一年的时间，换一辈子精彩 拷贝222",
+			"imgSrc"=>"images/list.png",
+			"content"=>"11年传统旅游从业者为什么全力以赴...",
+			"teacher"=>"主讲：王凯丽"
+		),
+		array(
+			"title"=>"用一年的时间，换一辈子精彩 拷贝333",
+			"imgSrc"=>"images/list.png",
+			"content"=>"11年传统旅游从业者为什么全力以赴...",
+			"teacher"=>"主讲：王凯丽"
+		),
+		array(
+			"title"=>"用一年的时间，换一辈子精彩 拷贝444",
+			"imgSrc"=>"images/list.png",
+			"content"=>"11年传统旅游从业者为什么全力以赴...",
+			"teacher"=>"主讲：王凯丽"
+		),
+		array(
+			"title"=>"用一年的时间，换一辈子精彩 拷贝555",
+			"imgSrc"=>"images/list.png",
+			"content"=>"11年传统旅游从业者为什么全力以赴...",
+			"teacher"=>"主讲：王凯丽"
+		),
+		array(
+			"title"=>"用一年的时间，换一辈子精彩 拷贝666",
+			"imgSrc"=>"images/list.png",
+			"content"=>"11年传统旅游从业者为什么全力以赴...",
+			"teacher"=>"主讲：王凯丽"
+		),
+		array(
+			"title"=>"用一年的时间，换一辈子精彩 拷贝777",
+			"imgSrc"=>"images/list.png",
+			"content"=>"11年传统旅游从业者为什么全力以赴...",
+			"teacher"=>"主讲：王凯丽"
+		),
+		array(
+			"title"=>"用一年的时间，换一辈子精彩 拷贝888",
+			"imgSrc"=>"images/list.png",
+			"content"=>"11年传统旅游从业者为什么全力以赴...",
+			"teacher"=>"主讲：王凯丽"
+		),
+		array(
+			"title"=>"用一年的时间，换一辈子精彩 拷贝999",
+			"imgSrc"=>"images/list.png",
+			"content"=>"11年传统旅游从业者为什么全力以赴...",
+			"teacher"=>"主讲：王凯丽"
+		),
+		array(
+			"title"=>"用一年的时间，换一辈子精彩 拷贝1010",
+			"imgSrc"=>"images/list.png",
+			"content"=>"11年传统旅游从业者为什么全力以赴...",
+			"teacher"=>"主讲：王凯丽"
+		),
+		array(
+			"title"=>"用一年的时间，换一辈子精彩 拷贝1111",
+			"imgSrc"=>"images/list.png",
+			"content"=>"11年传统旅游从业者为什么全力以赴...",
+			"teacher"=>"主讲：王凯丽"
+		),
+		array(
+			"title"=>"用一年的时间，换一辈子精彩 拷贝1212",
+			"imgSrc"=>"images/list.png",
+			"content"=>"11年传统旅游)业者为什么全力以赴...",
+			"teacher"=>"主讲：王凯丽"
+		),
+		array(
+			"title"=>"用一年的时间，换一辈子精彩 拷贝1313",
+			"imgSrc"=>"images/list.png",
+			"content"=>"11年传统旅游从业者为什么全力以赴...",
+			"teacher"=>"主讲：王凯丽"
+		),
+		array(
+			"title"=>"用一年的时间，换一辈子精彩 拷贝1414",
+			"imgSrc"=>"images/list.png",
+			"content"=>"11年传统旅游从业者为什么全力以赴...",
+			"teacher"=>"主讲：王凯丽"
+		),
+		array(
+			"title"=>"用一年的时间，)一辈子精彩 拷贝1515",
+			"imgSrc"=>"images/list.png",
+			"content"=>"11年传统旅游从业者为什么全力以赴...",
+			"teacher"=>"主讲：王凯丽"
+		),
+	)
+);
+
+echo json_encode($data);
